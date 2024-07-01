a5710a9cbb909da9c305aee0c756acc41802547f
hundalgkaur <125379419+hundalgkaur@users.noreply.github.com>
2024-07-01 17:42:13 +0530

    Sticky navabr

---
+++ js/resp.js
@@ -0,0 +1,13 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightNav = document.querySelector('.rightnav')
+navlist = document.querySelector('.nav-list')
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+
+
+
+})
